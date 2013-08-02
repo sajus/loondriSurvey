5a8a4e74f0124901c286ae680593294da293ab8d
snehalbhapkar <snehabhapkar88@gmail.com>
2013-08-02 20:30:15 +0530

    Added events in Login form

--- src/js/views/login/loginView.js
+++ src/js/views/login/loginView.js
@@ -1,8 +1,20 @@
-define(['backbone','text!templates/login/login.html'], function(Backbone, loginPageTemplate){
+define(['backbone','events','text!templates/login/login.html'], function(Backbone, Events, loginPageTemplate){
     
     var LoginPage = Backbone.View.extend({
         
         el: '.page',
+
+        events:{
+		      'click button[type=submit]': 'validateUser',
+		      'click button[type=button]': 'forgotMYPaswd'
+		    },
+
+		validateUser: function(){
+		    console.log('Submit button');
+		},
+		forgotMYPaswd: function(){
+		    console.log('Forgot my password');
+		 },
         
         render: function () {
             this.$el.html(loginPageTemplate);
