9da41334ec8c648a77faf105dc8ae7ab4f9ced89
pradnyajo <pradnyajo@pradnayajo.cybage.com>
2013-08-05 14:10:02 +0530

    Create user form validation done

--- src/js/models/user/userModel.js
+++ src/js/models/user/userModel.js
@@ -1,8 +1,28 @@
-define(['backbone'], function(Backbone) {
+define(['backbone','modelValidator'], function(Backbone) {
 
     var UserModel = Backbone.Model.extend({
 		defaults: {
 			title: 'users'
+		},
+		validation:{
+			userName:{
+				required: true,
+				msg: 'please enter user Name.'
+			},
+			userId: {
+				required: true,
+				pattern: 'number',
+				msg: 'Please enter valid User Id.'
+			},
+			userEmail:[{
+					required: true,
+					msg: 'Please enter Email Id.'
+				},
+				{
+					pattern: 'email',
+					msg: 'Email Id is not correct.'
+				}
+			]
 		}
     });
 
--- src/js/views/users/userView.js
+++ src/js/views/users/userView.js
@@ -1,16 +1,79 @@
-define(['jquery', 'underscore', 'backbone', 'template!templates/users/page','bootstrap','fuelux'], function($, _, Backbone, userPageTemplate){
+define(['jquery', 'underscore', 'backbone', 'template!templates/users/page', 'modelValidator','modelBinder','bootstrap','fuelux'], function($, _, Backbone, userPageTemplate,Validator){
 	
-	var User = require();
+	//var User = require();
 
     var userPage = Backbone.View.extend({
-
+		initialize: function(){
+			//instance of new modelBinder
+			
+			this.modelBinder = new Backbone.ModelBinder();
+		},
         el: '.page',
-
+		events: {
+			'submit .form-horizontal': 'processForm',
+			'blur input[type=text]':'processField',
+			'click #resetForm': 'resetForm'
+		},
+		 processField:function(e){
+            var target=$(e.target),fieldNameAttr=target.attr('name');
+            this.model.set(fieldNameAttr,target.val(),{validate:true});
+        },
+		processForm: function(e){
+			e.preventDefault();
+			if(this.model.isValid(true)){
+				this.$('.alert-success').removeClass('hide').fadeIn();	
+				this.$('.alert-error').addClass('hide').fadeOut();	
+			}
+			else{
+				console.log('validation');
+				this.$('.alert-error').removeClass('hide').fadeIn();
+				this.$('.alert-success').addClass('hide').fadeOut();	
+			}
+		},
+		resetForm: function(e){
+			e.preventDefault();
+			this.$('.form-horizontal').find("input[type=text], textarea, select").val("");
+			this.hideError(view, attr, error);
+		},
         render: function () {
             this.$el.html(userPageTemplate);
-			console.log(this.model.toJSON());
-			$('#userGrid').datagrid({dataSource: null, stretchHeight: true});
-        }
+			//binding modelbinder to el and model together
+			this.modelBinder.bind(this.model, this.el);
+			
+			
+			// validation binding
+			Backbone.Validation.bind(this,{
+				invalid: this.showError,
+				valid: this.hideError
+			});
+			return this;
+			//$('#userGrid').datagrid({dataSource: null, stretchHeight: true});
+        },
+		showError: function(view, attr, error){
+			// showing errors on UI
+			console.log(error);
+			var targetView = view.$el,
+            targetSelector = targetView.find("[name="+attr+"]"),
+            targetParent = targetSelector.closest(".control-group"),
+			errorSpan = targetParent.find('.help-inline');
+			//adding error message to errorspan
+			if($.trim(errorSpan.html())===''){
+				errorSpan.append(error);
+			}else if(errorSpan.html().toLowerCase()!==error.toLowerCase()){
+                errorSpan.html(error);
+            }
+			
+			targetParent.addClass('error');
+		},
+		hideError: function(view, attr, error){
+			// hiding errors on UI
+			console.log(error);
+			var targetView = view.$el,
+            targetSelector = targetView.find("[name="+attr+"]"),
+            targetParent = targetSelector.closest(".control-group");
+			targetParent.find('.help-inline').html('');
+			targetParent.removeClass('error');
+		}
     });
 
     return userPage;
