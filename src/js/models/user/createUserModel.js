edfa87bf9836fbf3566286dfad6091440d4a1b85
ashwinh <ashwinh@cybage.com>
2013-08-14 12:50:04 +0530

    Removed password on user form.
    Signed-off-by: ashwinh <ashwinh@cybage.com>

--- src/js/models/user/createUserModel.js
+++ src/js/models/user/createUserModel.js
@@ -2,31 +2,7 @@ define(['backbone', 'globals', 'modelValidator'], function(Backbone, globals) {
 
     var UserModel = Backbone.Model.extend({
 		url: function(){
-			return globals.gateWayUrl + "/getAllDesignations";
-		},
-
-		validation:{
-			empid: {
-				required: true,
-				pattern: 'number',
-				msg: 'Employee ID can\'t be empty'
-			},
-			firstname: {
-				required: true,
-				msg: 'First name can\'t be emply'
-			},
-			lastname: {
-				required: true,
-				msg: 'Last name can\'t be emply'
-			},
-			email: {
-				required: true,
-				msg: 'Email can\'t be emply'
-			},
-			password: {
-				required: true,
-				msg: 'Password can\'t be emply'
-			}
+			return globals.gateWayUrl + "/createOrUpdateUsers";
 		}
     });
 
--- src/js/views/users/userView.js
+++ src/js/views/users/userView.js
@@ -1,5 +1,5 @@
 define(['jquery', 'underscore', 'views/BaseView', 'backbone', 'events','template!templates/users/page', 'modelValidator', 'models/user/getDesignationModel' , 'collections/users/usersCollection', 'modelBinder','bootstrap','fuelux'], 
-	function($, _, BaseView, Backbone,Events, userPageTemplate, Validator, DesignationModel, ModifyCollection){
+	function($, _, BaseView, Backbone, Events, userPageTemplate, Validator, DesignationModel, ModifyCollection){
 
     return BaseView.extend({
 
@@ -13,51 +13,8 @@ define(['jquery', 'underscore', 'views/BaseView', 'backbone', 'events','template
 
 		events: {
 			'click #addUser': 'processForm',
-			'blur input[type=text], select, input[type=checkbox], input[type=radio], change :input': 'processField',
-			'click .keygen': 'keygen',
+			'blur select, change :input': 'processField',
 			'click #resetForm': 'resetForm'
-		},
-
-		keygen: function() {
-			var specials = '!@#$%^&*()_+{}:"<>?\|[];\',./`~';
-			var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-			var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-			var numbers = '0123456789';
-
-			var all = specials + lowercase + uppercase + numbers;
-
-			String.prototype.pick = function(min, max) {
-			    var n, chars = '';
-
-			    if (typeof max === 'undefined') {
-			        n = min;
-			    } else {
-			        n = min + Math.floor(Math.random() * (max - min));
-			    }
-
-			    for (var i = 0; i < n; i++) {
-			        chars += this.charAt(Math.floor(Math.random() * this.length));
-			    }
-
-			    return chars;
-			};
-
-			String.prototype.shuffle = function() {
-			    var array = this.split('');
-			    var tmp, current, top = array.length;
-
-			    if (top) while (--top) {
-			        current = Math.floor(Math.random() * (top + 1));
-			        tmp = array[current];
-			        array[current] = array[top];
-			        array[top] = tmp;
-			    }
-
-			    return array.join('');
-			};
-
-			var password = (specials.pick(1) + lowercase.pick(1) + uppercase.pick(1) + all.pick(3, 10)).shuffle();
-			$('#password').val(password);
 		},
 
 		resetForm: function(e){
@@ -97,10 +54,10 @@ define(['jquery', 'underscore', 'views/BaseView', 'backbone', 'events','template
 		postData: function(){
 			this.model.save({
 				success: function() {
-					console.log("Created User on Server");
+					Events.trigger('alert:success',[{message:"User created successfully."}]);
 				},
 				error: function() {
-					console.log("Create User Services Error");
+					Events.trigger('alert:error',[{message:"Some service error occured during data fetching"}]);
 				}
 			});
 		}
