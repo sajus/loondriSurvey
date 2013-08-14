853ec8a01626128d0c266a73a050f3731a3b94d1
ashwinh <ashwinh@cybage.com>
2013-08-14 16:44:30 +0530

    Create user service update;
    Signed-off-by: ashwinh <ashwinh@cybage.com>

--- src/js/models/user/createUserModel.js
+++ src/js/models/user/createUserModel.js
@@ -2,8 +2,11 @@ define(['backbone', 'globals', 'modelValidator'], function(Backbone, globals) {
 
     var UserModel = Backbone.Model.extend({
 		url: function(){
-			return globals.gateWayUrl + "/createOrUpdateUsers";
-		}
+			return globals.gateWayUrl + "/createUsers";
+		},
+		validation: {
+
+        }
     });
 
     return UserModel;
