f268b4431957bec6e04a1f1a787b972deef1a2d9
Vinayak Patil <vinayakpat@cybage.com>
2013-08-21 18:10:19 +0530

    adds options collection and updates category model

--- src/js/models/survey/wizard/categoryDetails.js
+++ src/js/models/survey/wizard/categoryDetails.js
@@ -1,6 +1,32 @@
-define(['backbone', 'modelValidator', 'jqueryCookie'], function(Backbone) {
+define(['backbone', 'collections/survey/options', 'modelValidator', 'jqueryCookie'], function(Backbone, OptionsCollection) {
     return Backbone.Model.extend({
-        initialize: function() {},
+        initialize: function() {
+            this.optionsCollection = new OptionsCollection();
+            console.log(this.optionsCollection);
+        },
+        fetchOptions: function() {
+            var self=this;
+            if (this.get('categorytype') === 'other') {
+                return;
+            } else {
+                $.ajax({
+                    async: false,
+                    url: Backbone.Model.gateWayUrl + "/getOptionsByCategoryId",
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    data: JSON.stringify({
+                        categoriesid: this.get('categoriesid')
+                    }),
+                    success: function(data, response) {
+                        self.optionsCollection.reset(data.Optionslist);
+                    },
+                    error:function(){
+                        console.log("in the error of fetch options");
+                    }
+                });
+
+            }
+        },
         validation: {
             categoryname: 'validateCategory',
             categorytype: {
@@ -9,8 +35,6 @@ define(['backbone', 'modelValidator', 'jqueryCookie'], function(Backbone) {
             }
         },
         validateCategory: function(value, attr, computedState) {
-            console.log(value);
-            console.log(computedState);
             if ($.cookie("isNoCategory") === "true") {
                 this.set('categoryname', 'NA');
             } else {
