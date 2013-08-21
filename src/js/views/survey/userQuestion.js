6ad18a13b89e6807c6c458d178a1672fedde75aa
JerinJ <jerinj@cybage.com>
2013-08-21 22:14:41 +0530

    adding pending file
    Signed-off-by: JerinJ <jerinj@cybage.com>

--- src/js/views/survey/userQuestion.js
+++ src/js/views/survey/userQuestion.js
@@ -1,16 +1,29 @@
 define(function(require) {
     var Backbone = require('backbone'),
-        questionTemplate = require('template!templates/survey/userQuestion');
+        questionTemplate = require('template!templates/survey/userQuestion'),
+        CategoryModel = require('models/survey/wizard/categoryDetails');
     /* Requires with no assignment */
     return Backbone.View.extend({
         className: 'accordion-group',
         initialize: function() {
-
+            this.model.fetchCategories(this.model.get('questionid'));
         },
-        events: {},
+        events: {
+            'change #category':'selectChoice'
+        },
         render: function() {
-            this.$el.html(surveyDetailedTemplate(this.model.toJSON()));
+            this.$el.html(questionTemplate({
+                question: this.model.toJSON(),
+                category: this.model.categories.toJSON()
+            }));
             return this;
+        },
+        selectChoice: function() {
+            var $Id = $('#category').find('option:selected').val();
+            var $Type = $('#category').find('option:selected').attr('type');
+            this.CategoryView = require('views/survey/userCategoryView');
+            var categoryView = new this.CategoryView({categoryId:$Id, categoryType:$Type});
+            this.$('.userCategory').html(categoryView.render().el);
         }
     });
 
